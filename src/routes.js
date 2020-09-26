2a7ed3fdf6aa290e2ad6b84b4b1f52812f87bea6
Fulano2141 <macuto2142dt@gmail.com>
2020-09-25 23:45:02 -0400

    routes generic

--- src/routes.js
+++ src/routes.js
@@ -3,17 +3,32 @@ import Home from './components/Home'
 import UserStart from './components/user/UserStart'
 import UserEdit from './components/user/UserEdit'
 import UserDetail from './components/user/UserDetail'
-
+import Header from './components/Header'
 export const routes = [
-    { path: '/', component: Home, name: 'home' },
+    {
+        path: '/', name: 'home', components: {
+            default: Home,
+            'header-top': Header
+        }
+    },
     // { path: '/user/:id', component: User },
     {
         path: '/user',
-        component: User,
+        components: {
+            default: User,
+            'header-bottom': Header
+        },
         children: [
             { path: '', component: UserStart },
             { path: ':id', component: UserDetail },
             { path: ':id/edit', component: UserEdit, name: 'userEdit' }
         ],
     },
+    // try http://localhost:8080/redirect-me with this 
+    { path: '/redirect-me', redirect: '/user' },
+    // or this
+    // { path: '/redirect-me', redirect: { name: 'home' } },
+
+    // Redirect everything
+    { path: '*', redirect: { name: 'home' } }
 ];
